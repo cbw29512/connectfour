2fd3cc07e0535ff719254a0f69a45da5c17fc398
chris <cbw29512@outlook.com>
2019-09-13 11:41:28 -0400

    updated files

---
+++ javascript.js
@@ -0,0 +1,29 @@
+let board [
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,    
+];
+
+const edgeX = board[0].length - 2;
+const edgeY = board.length - 2;
+
+function checkHorizontal {
+}
+
+function checkVertical{
+    
+}
+function searchdiagonalLeft {
+    
+}
+function searchDiagonalRight {
+    
+}
+
+wincondition {
+
+    
+}
