9482d22fc1089c93b0bc44490bf1fe28f7848b11
chris <cbw29512@outlook.com>
2019-09-19 09:41:27 -0400

    updated javascript

--- javascript.js
+++ javascript.js
@@ -1,67 +1,156 @@
-let currentTurn = 'player1';
+let currentTurn = "player1";
 
-const columns = document.getElementsByTagName('tr');
+const columns = document.getElementsByTagName("tr");
 
 for (let columnIndex = 0; columnIndex < columns.length; columnIndex++) {
-    columns[columnIndex].addEventListener('click', dropChip);
+  columns[columnIndex].addEventListener("click", dropChip);
 }
 
 function dropChip() {
-    let column = event.currentTarget
-    let cells = column.children
-    for (let cellIndex = cells.length - 1; cellIndex >= 0;cellIndex--) {
+  let column = event.currentTarget;
+  let cells = column.children;
+  for (let cellIndex = cells.length - 1; cellIndex >= 0; cellIndex--) {
+    if (cells[cellIndex].childElementCount === 0) {
+      const chip = document.createElement("div");
 
-        console.log(row)
-        if (cells[cellIndex].childElementCount === 0) {
+      if (currentTurn === "player1") {
+        chip.className = "chipRed";
+      } else if (currentTurn == "player2") {
+        chip.className = "chipBlack";
+      }
 
-            const chip = document.createElement('div');
+      cells[cellIndex].appendChild(chip);
+      break;
+    }
+  }
+  let condition = checkEndCondition();
 
-            if (currentTurn === 'player1') {
-                chip.className = 'chipRed';
-            } else if (currentTurn == 'player2') {
-                chip.className = 'chipBlack';
-            };
 
-            cells[cellIndex].appendChild(chip);
-            break;
-        };
-    };
-    let condition = checkEndCondition () 
 
-};
-    function checkEndCondition () {
-        if ( checkHorizontalWin () || checkVerticialWin() || checkDiagonalLeft() || checkDiagonalright()) {
-            return "win"   
-        }  else if (checkTie()) {
+}
+
+function checkEndCondition() {
+  if (checkHorizontalWin() || checkVerticialWin() || checkDiagonalLeft() || checkDiagonalRight()) {
+    return "win";
+  } /*else if (checkTie()) {
             return "tie"
         }  else {
             return
+        }*/
+}
+
+function checkHorizontalWin() {
+
+  const xEdge = (columns.length - 3); 
+
+  for (let columnIndex = 0; columnIndex < xEdge; columnIndex++) {
+    let cells = columns[columnIndex].children;
+
+    for (let cellIndex = 0; cellIndex < cells.length; cellIndex++) {
+
+      if (cells[cellIndex].hasChildNodes()) {
+        let firstCell = columns[columnIndex].children[cellIndex];
+        let secondCell = columns[columnIndex + 1].children[cellIndex];
+        let thirdCell = columns[columnIndex + 2].children[cellIndex];
+        let fourthCell = columns[columnIndex + 3].children[cellIndex];
+
+        if (
+          secondCell.hasChildNodes() &&
+          secondCell.className === firstCell.className &&
+          (thirdCell.hasChildNodes() &&
+            thirdCell.className === firstCell.className) &&
+          (fourthCell.hasChildNodes() &&
+            fourthCell.className === firstCell.className)
+        ) {
+          return true;
         }
+      }
     }
+  }
+  return false;
+};
 
-    function checkHorizontalWin () {
-        for ( let columnIndex = 0; columnIndex < columns.length; columnIndex++) {
-            let cells = columns[columnIndex].children
-                for (let cellIndex = 0 ; cellIndex < cells.length; cellIndex++) {
-                    if ( cells [cellIndex].hasChildNodes()) {
-                        
-                }
+function checkVerticalWin() {
 
-            }
+    let row = columns[0].children;
+    const yEdge = (row.length - 3); 
+  
+    for (let columnIndex = 0; columnIndex < columns.length; columnIndex++) {
+      let cells = columns[columnIndex].children;
+  
+      for (let rowIndex = 0; rowIndex < yEdge; rowIndex++) {
 
+        if (cells[rowIndex].hasChildNodes()) {
+          let firstCell = columns[columnIndex].children[rowIndex];
+          let secondCell = columns[columnIndex].children[rowIndex + 1];
+          let thirdCell = columns[columnIndex].children[rowIndex + 2];
+          let fourthCell = columns[columnIndex].children[rowIndex + 3];
+  
+          if ((secondCell.hasChildNodes() && secondCell.className === firstCell.className) &&
+            (thirdCell.hasChildNodes() && thirdCell.className === firstCell.className) &&
+            (fourthCell.hasChildNodes() && fourthCell.className === firstCell.className))
+          {
+            return true;
+          }
         }
-    }        
+      }
+    }
+    return false;
+};
 
-           
-        
-        
-        
-        /*if horzontial || vertical || diagonalL || diagonalR 
-        return 
+function checkDiagonalLeft() {
 
-        else tie 
-        return
+    const xEdge = (columns.length - 3); 
+    let row = columns[0].children;
+    const yEdge = (row.length - 3);
+  
+    for (let columnIndex = 0; columnIndex < xEdge; columnIndex++) {
+      let cells = columns[columnIndex].children;
+  
+      for (let rowIndex = 0; rowIndex < cells.length; rowIndex++) {
+  
+        if (cells[rowIndex].hasChildNodes()) {
+          let firstCell = columns[columnIndex].children[rowIndex];
+          let secondCell = columns[columnIndex + 1].children[rowIndex + 1];
+          let thirdCell = columns[columnIndex + 2].children[rowIndex + 2];
+          let fourthCell = columns[columnIndex + 3].children[rowIndex + 3];
+  
+          if ((secondCell.hasChildNodes() && secondCell.className === firstCell.className) &&
+             (thirdCell.hasChildNodes() && thirdCell.className === firstCell.className) &&
+             (fourthCell.hasChildNodes() && fourthCell.className === firstCell.className)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+};
 
-        else
-        return
-    }*/
+function checkDiagonalRight() {
+    const countStart = columns.length - 1;
+
+    const xEdge = (columns.length - 3); 
+    let row = columns[0].children;
+    const yEdge = (row.length - 3);
+  
+    for (let columnIndex = countStart ; columnIndex > 2 ; columnIndex--) {
+      let cells = columns[columnIndex].children;
+  
+      for (let rowIndex = 0; rowIndex < cells.leng; rowIndex++) {
+  
+        if (cells[rowIndex].hasChildNodes()) {
+          let firstCell = columns[columnIndex].children[rowIndex];
+          let secondCell = columns[columnIndex - 1].children[rowIndex + 1];
+          let thirdCell = columns[columnIndex - 2].children[rowIndex + 2];
+          let fourthCell = columns[columnIndex - 3].children[rowIndex + 3];
+  
+          if ((secondCell.hasChildNodes() && secondCell.className === firstCell.className) &&
+             (thirdCell.hasChildNodes() && thirdCell.className === firstCell.className) &&
+             (fourthCell.hasChildNodes() && fourthCell.className === firstCell.className)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+};
